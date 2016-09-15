b39707e3d2f7a987607c197a8015c5eeb2de0ba9
Dhinesh Kumar Devanathan <dhineshdevanathan@gmail.com>
2016-09-15 01:03:52 -0400

    finished assignment 1

--- streams/src/main/scala/streams/Solver.scala
+++ streams/src/main/scala/streams/Solver.scala
@@ -10,7 +10,7 @@ trait Solver extends GameDef {
   /**
    * Returns `true` if the block `b` is at the final position
    */
-  def done(b: Block): Boolean = ???
+  def done(b: Block): Boolean = goal == b.b1 && goal == b.b2
 
   /**
    * This function takes two arguments: the current block `b` and
@@ -28,7 +28,11 @@ trait Solver extends GameDef {
    * It should only return valid neighbors, i.e. block positions
    * that are inside the terrain.
    */
-  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] = ???
+  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] =  {
+    (for {
+      (b, m) <- b.legalNeighbors
+    } yield (b,m :: history)).toStream
+  }
 
   /**
    * This function returns the list of neighbors without the block
@@ -36,7 +40,16 @@ trait Solver extends GameDef {
    * make sure that we don't explore circular paths.
    */
   def newNeighborsOnly(neighbors: Stream[(Block, List[Move])],
-                       explored: Set[Block]): Stream[(Block, List[Move])] = ???
+                       explored: Set[Block]): Stream[(Block, List[Move])] = {
+    if (neighbors.isEmpty) Stream.empty
+    else {
+      val more = for {
+        (b,m) <- neighbors
+        if !(explored contains b)
+      } yield (b,m)
+      more.toStream
+    }
+  }
 
   /**
    * The function `from` returns the stream of all possible paths
@@ -62,18 +75,37 @@ trait Solver extends GameDef {
    * construct the correctly sorted stream.
    */
   def from(initial: Stream[(Block, List[Move])],
-           explored: Set[Block]): Stream[(Block, List[Move])] = ???
+           explored: Set[Block]): Stream[(Block, List[Move])] = {
+    if ( initial.isEmpty) Stream.empty
+    else {
+      val more = for {
+        (b,m) <- initial
+        next <- newNeighborsOnly(neighborsWithHistory(b,m), explored)
+      } yield next
+      initial ++ from(more, explored ++ (more map (_._1)))
+    }
+  }
 
   /**
    * The stream of all paths that begin at the starting block.
    */
-  lazy val pathsFromStart: Stream[(Block, List[Move])] = ???
+  lazy val pathsFromStart: Stream[(Block, List[Move])] = {
+    val block = startBlock
+    val moves = List()
+    val explored = Set(block)
+    from((block,moves) #:: Stream.empty, explored)
+  }
 
   /**
    * Returns a stream of all possible pairs of the goal block along
    * with the history how it was reached.
    */
-  lazy val pathsToGoal: Stream[(Block, List[Move])] = ???
+  lazy val pathsToGoal: Stream[(Block, List[Move])] = {
+    for {
+      path <- pathsFromStart
+      if done(path._1)
+    } yield path
+  }
 
   /**
    * The (or one of the) shortest sequence(s) of moves to reach the
@@ -83,5 +115,8 @@ trait Solver extends GameDef {
    * the first move that the player should perform from the starting
    * position.
    */
-  lazy val solution: List[Move] = ???
+  lazy val solution: List[Move] = pathsToGoal match  {
+    case x #:: _ => x._2.reverse
+    case _ => Nil
+  }
 }
--- streams/src/main/scala/streams/test.sc
+++ streams/src/main/scala/streams/test.sc
@@ -3,20 +3,22 @@
 case class Pos(row: Int, col: Int)
 
 
-val v = Vector(Vector('S', 'T'), Vector('o', 'o'), Vector('o', 'o'), Vector('o', 'b'))
+val levelVector = Vector(Vector('S', 'T'), Vector('o', 'o'), Vector('o', 'o'), Vector('o', 'b'), Vector('-', '-') )
 
 val c = 'b'
-val row = v.indexWhere( x => x.indexOf(c)  != -1)
-val col = v(row).indexOf(c)
+val row = levelVector.indexWhere( x => x.indexOf(c)  != -1)
+val col = levelVector(row).indexOf(c)
 
 
 def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = {
-def theTerrainFunction(p: Pos) : Boolean = {
-if (levelVector.indices.exists(_ == p.row)) levelVector(p.row).indices.exists(_ == p.col)
-else false
-}
-theTerrainFunction
+  def theTerrainFunction(p: Pos) : Boolean = {
+    if (levelVector.indices.exists(_ == p.row) && levelVector(p.row).indices.exists(_ == p.col) && levelVector(p.row)(p.col) != '-' ) true
+    else false
+  }
+  theTerrainFunction
 }
 
-terrainFunction(v)(new Pos(10,10))
-terrainFunction(v)(new Pos(1,1))
+terrainFunction(levelVector)(new Pos(10,10))
+terrainFunction(levelVector)(new Pos(4,1))
+terrainFunction(levelVector)(new Pos(1,1))
+terrainFunction(levelVector)(new Pos(2,0))
